c51a8744c577f456914fd1fa9998285a1c8d9a93
K0chanRus <144388877+K0chanRus@users.noreply.github.com>
2024-04-03 19:38:48 +0300

    End

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,63 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        String[] picnic_array = openFile();
+        String maxNamePicnic = maxName(picnic_array);
+        System.out.print("The word with the most letters: ");
+        System.out.println(maxNamePicnic);
+        HashMap<String, Integer> picnic = countPicnic(picnic_array);
+        picnic.forEach((key, value) -> System.out.println(key + " :" + value));
+    }
 
+    public static String[] openFile(){
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"))){
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] picnic_array = line.split(" ");
+                ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(picnic_array));
+                for (int i = 0; i < arrayList.size(); i++) {
+                    arrayList.remove("");
+                }
+                String[] array = arrayList.toArray(new String[0]);
+                return array;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public  static String maxName(String[] array){
+        int max = 0;
+        String name = null;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length() > max){
+                max = array[i].length();
+                name = array[i];
+            }
+        }
+        return name;
+    }
+    
+    public static HashMap<String, Integer> countPicnic(String[] array){
+        HashMap<String, Integer> picnic = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (picnic.containsKey(array[i])){
+                int vale = picnic.get(array[i]) + 1;
+                picnic.put(array[i], vale);
+            } else {
+                picnic.put(array[i], 1);
+            }
+        }
+        return picnic;
     }
 }
